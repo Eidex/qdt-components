c73dd2fc2a788888bba2331f95d11819a765cd0e
David Jones <djones@eidexinsights.com>
2018-05-08 14:39:58 -0400

    again again

--- src/components/QdtViz.jsx
+++ src/components/QdtViz.jsx
@@ -104,9 +104,9 @@ export default class QdtViz extends React.Component {
     if (this.state.error) {
       return <div>{this.state.error.message}</div>;
     } else if (this.state.loading) {
-      return <div style={{ 'text-align': 'center', height: '200px' }}><img src="./Ellipsis-6.2s-200px.svg" alt="" /></div>;
+      return <div style={{ 'text-align': 'center', height: '200px' }}><img src="./Ellipsis-6.2s-189px.gif" alt="" /></div>;
     }
     const { width, height } = this.props;
-    return <div ref={(node) => { this.node = node; }} style={{ width, height }} onClick={() => { console.log('qdt clicked!'); }}><img src="./Ellipsis-6.2s-189px.gif" alt="" /></div>;
+    return <div ref={(node) => { this.node = node; }} style={{ width, height }} onClick={() => { console.log('qdt clicked!'); }} />;
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -65,7 +65,7 @@ module.exports = {
         ]
       },
         {
-          test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          test: /\.(ttf|eot|svg|gif)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
           use: [
             { loader: 'file-loader' }
           ]
