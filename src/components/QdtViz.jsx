3b10d5db0f60113029b0a080347b7b9970aaa0ba
David Jones <djones@eidexinsights.com>
2018-05-08 14:03:04 -0400

    ripples 2

--- src/components/QdtViz.jsx
+++ src/components/QdtViz.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import FontAwesome from 'react-fontawesome';
+// import FontAwesome from 'react-fontawesome';
 
 export default class QdtViz extends React.Component {
   static propTypes = {
