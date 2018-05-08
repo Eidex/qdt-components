b9cf2c292aa1de2c54b08ecf7dc1bf510fdf5213
David Jones <djones@eidexinsights.com>
2018-05-08 14:43:17 -0400

    no svg

--- src/components/QdtViz.jsx
+++ src/components/QdtViz.jsx
@@ -104,7 +104,7 @@ export default class QdtViz extends React.Component {
     if (this.state.error) {
       return <div>{this.state.error.message}</div>;
     } else if (this.state.loading) {
-      return <div style={{ 'text-align': 'center', height: '200px' }}><img src="./Ellipsis-6.2s-189px.gif" alt="" /></div>;
+      return <div style={{ 'text-align': 'center', height: '200px' }}>woork</div>;
     }
     const { width, height } = this.props;
     return <div ref={(node) => { this.node = node; }} style={{ width, height }} onClick={() => { console.log('qdt clicked!'); }} />;
